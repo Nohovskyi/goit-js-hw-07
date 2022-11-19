50b1c4371984f464db797144a96a2a69f12c4244
Nohovskyi <nogovskiy@gmail.com>
2022-11-19 19:51:26 +0200

    Додає перше завдання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const cardMarkup = createImageCard(galleryItems);
+
+function createImageCard(galleryItems) {
+  return galleryItems
+    .map((item) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+gallery.insertAdjacentHTML("beforeend", cardMarkup);
+
+gallery.addEventListener("click", onLinkContainerClick);
+
+
+function onLinkContainerClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  console.log(event.target.dataset.source);
+  openModal(event);
+
+    
+    // if (basicLightbox.__esModule) {
+    //     document.addEventListener("keydown", event => {
+    //         if (event.code === "Escape") {
+    //          return basicLightbox.__esModule;
+    //         }
+    //     });
+    // };
+}
+
+function openModal(event) {
+  basicLightbox
+    .create(
+      `
+		<img width="1400" height="900" src="${event.target.dataset.source}">
+	`
+    )
+    .show();
+}
